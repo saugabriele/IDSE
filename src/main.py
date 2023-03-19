9f7e66a2ae5dc7f761635105aafc9d35a2d357a7
saugabriele <gabriele.sau2598@gmail.com>
2023-03-19 11:22:33 +0100

    First commit from pycharm

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 from utils import load_mnist_data
+from pandas import read_csv
 
-x, y = load_mnist_data()
-print(x.shape, y.shape)
+# x, y = load_mnist_data(
+data = read_csv("C:/Users/Utente/Desktop/mnist_data.csv")
+print(data.shape)
