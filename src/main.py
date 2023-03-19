f27cd75ef408c77fb8db665dfaf02956ca61d3be
saugabriele <gabriele.sau2598@gmail.com>
2023-03-19 17:36:50 +0100

    Final project

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 import numpy as np
 from utils import *
 from nmc import NMC
+from sklearn.model_selection import ShuffleSplit
 
-n_rep = 5
 x, y = load_mnist_data()
 
-test_error = np.zeros(shape=(n_rep,))
-for i in range(n_rep):
-    x_tr, y_tr, x_ts, y_ts = split_data(x, y, n_tr=1000)
+splitter = ShuffleSplit(n_splits=5, train_size=.5, random_state=0)
+
+test_error = np.zeros(shape=(splitter.n_splits,))
+for i, (tr_idx, ts_idx) in enumerate(splitter.split(x, y)):
+    # x_tr, y_tr, x_ts, y_ts = split_data(x, y, n_tr=1000)
+    x_tr, y_tr = x[tr_idx, :], y[tr_idx]
+    x_ts, y_ts = x[ts_idx, :], y[ts_idx]
     clf = NMC(robust_centroid_estimation=True)
     clf.fit(x_tr, y_tr)
     # plot_ten_digits(clf.centroids)
