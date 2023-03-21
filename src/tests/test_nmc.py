ef1dc6310729c6170d25ce52223bf12abf842881
saugabriele <gabriele.sau2598@gmail.com>
2023-03-21 18:11:46 +0100

    Added basic skeleton for testing

--- src/tests/test_nmc.py
+++ src/tests/test_nmc.py
@@ -10,10 +10,22 @@ class TestNMC(unittest.TestCase):
         n_samples = 100
         n_features = 20
         self.x = np.zeros(shape=(n_samples, n_features))
-        self.y = np.ones(shape=(n_samples, ))
+        self.y = np.zeros(shape=(n_samples, ))
+        self.y[50:] = 1
+        self.clf = NMC()
+
+    def test_init(self):
+        """Checking if the centroids are None right after creation."""
+        self.assertTrue(self.clf.centroids is None)
+
+    def test_robust_estimation(self):
+        self.assertRaises(TypeError, setattr, self.clf, 'robust_centroid_estimation', 'achavcvah')
 
     def test_fit(self):
-        pass
+        n_classes = np.unique(self.y).size
+        expected_centroid_shape = (n_classes, self.x.shape[1])
+        self.clf.fit(self.x, self.y)
+        self.assertEqual(self.clf.centroids.shape, expected_centroid_shape)
 
     def test_predict(self):
         pass
